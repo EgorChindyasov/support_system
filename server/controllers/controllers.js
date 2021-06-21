e20099aa516e2b9ea27f247ca13d356ec60656f8
Egor O. Chindyasov <egor@chindyasov.ru>
2021-06-21 19:07:39 +0300

    add route /closed

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -7,15 +7,33 @@ class DataController {
         res.json(data.rows)
     }
 
-    async deleteData(req, res) {
+    async closeData(req, res) {
         const id = req.params.id
         const data = await db.query('delete from data where id = $1', [id])
         res.json(data.rows)
     }
 
-    async insertDeleteData(req, res) {
+    async insertClosedData(req, res) {
         const {name, content, isitimportant} = req.body
         const insertData = await db.query('insert into delete_data (name, content, isitimportant) values ($1, $2, $3) returning *', 
+                                         [name, content, isitimportant])
+        res.json(insertData.rows)
+    }
+
+    async getClosedData(req, res) {
+        const data = await db.query('select * from delete_data')
+        res.json(data.rows)
+    }
+
+    async deleteFromClosed(req, res) {
+        const id = req.params.id
+        const data = await db.query('delete from delete_data where id = $1', [id])
+        res.json(data.rows)
+    }
+
+    async insertIntoHome(req, res) {
+        const {name, content, isitimportant} = req.body
+        const insertData = await db.query('insert into data (name, content, isitimportant) values ($1, $2, $3) returning *', 
                                          [name, content, isitimportant])
         res.json(insertData.rows)
     }
--- server/routers/routers.js
+++ server/routers/routers.js
@@ -5,7 +5,10 @@ const router = new Router()
 const dataController = require('../controllers/controllers')
 
 router.get('/server', dataController.getData)
-router.delete('/delete/:id', dataController.deleteData)
-router.post('/server', dataController.insertDeleteData)
+router.delete('/server/:id', dataController.closeData)
+router.post('/server', dataController.insertClosedData)
+router.get('/closed', dataController.getClosedData)
+router.delete('/closed/:id', dataController.deleteFromClosed)
+router.post('/closed', dataController.insertIntoHome)
 
 module.exports = router
