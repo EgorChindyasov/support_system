cdbf91a3442dfdd1972d69178508df73f7476fa7
Egor O. Chindyasov <egor@chindyasov.ru>
2021-06-21 19:14:15 +0300

    changed component Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,6 @@ function Home ( {id, name, content, importance} ) {
   // состояние отображения блока
   const [showBlock, setShowBlock] = useState(true)
 
-  const [status, setStatus] = useState(null);
-
   const display = () => setShowBlock(prev => !prev) 
     
   const requestOptions = {
@@ -20,17 +18,11 @@ function Home ( {id, name, content, importance} ) {
 
   if (!showBlock) {
       
-    fetch(`http://localhost:8080/api/delete/${id}`, { method: 'DELETE' })
-    // .then(() => setStatus('Delete successful'))
+    fetch(`http://localhost:8080/api/server/${id}`, { method: 'DELETE' })
     fetch('/api/server', requestOptions)
       
     return null
   }
-
-  // if (showBlock === false) {
-  //   fetch('/api/server', requestOptions)
-  //     .then(response => response.json())
-  // }
 
     return (
       <div className='AppBlock'>
@@ -45,3 +37,95 @@ function Home ( {id, name, content, importance} ) {
 }
 
 export default Home
+
+
+// import React, { useState, useEffect} from 'react'
+// import InformBlock from '../components/InformBlock/InformBlock'
+
+// function Home () {
+//   // состояние отображения блока
+//   const [showBlock, setShowBlock] = useState(true)
+
+//   const [error, setError] = useState(null)
+//   const [isLoaded, setIsLoaded] = useState(false)
+//   const [items, setItems] = useState([])
+
+//   const display = () => setShowBlock(prev => !prev)
+
+//   useEffect(() => {
+//     fetch('/api/server')
+//       .then(res => res.json())
+//       .then(
+//         (result) => {
+//           setIsLoaded(true)
+//           setItems(result)
+//         },
+//         (error) => {
+//           setIsLoaded(true)
+//           setError(error)
+//         }
+//       )
+//   }, [])
+
+//   if (error) {
+//     return <div>Ошибка: {error.message}</div>
+//   } 
+  
+//   else if (!isLoaded) {
+//     return <div>Загрузка...</div>
+//   } 
+//   else {
+//     let MessageComponent = items.map(message => {
+//       return (
+//         <InformBlock 
+//           id={message.id}
+//           name={message.name} 
+//           content={message.content} 
+//           importance={message.isitimportant} 
+//           display={display}
+//           key={message.id} 
+//         />
+//       )
+//     })
+ 
+    
+//   // const requestOptions = {
+//   //   method: 'POST',
+//   //   headers: {'Content-Type':'application/json'},
+//   //   body: JSON.stringify({ 
+//   //     name: `${name}`,
+//   //     content: `${content}`,
+//   //     isitimportant: `${importance}` })
+//   // }
+
+//   if (!showBlock) {
+//     // items.map(id => fetch(`http://localhost:8080/api/closed/${id.id}`, { method: 'DELETE' }))  
+//     // // fetch(`http://localhost:8080/api/closed/${id}`, { method: 'DELETE' })
+//     // fetch('/api/server', requestOptions)
+      
+//     return null
+//   }
+  
+//   // else {
+//   // let MessageComponent = items.map(message => {
+//   //   return (
+//   //     <InformBlock 
+//   //       id={message.id}
+//   //       name={message.name} 
+//   //       content={message.content} 
+//   //       importance={message.isitimportant} 
+//   //       display={display}
+//   //       key={message.id} 
+//   //     />
+//   //   )
+//   // })
+
+//     return (
+//       <div className='AppBlock'>
+//         {MessageComponent}
+//       </div>
+//     )
+// }
+// }
+
+// export default Home
