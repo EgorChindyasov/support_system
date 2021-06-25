8153497047cc4682b59f8ed9f5cd67d06def8049
Egor O. Chindyasov <egor@chindyasov.ru>
2021-06-25 19:32:10 +0300

    added hide routers

--- server/routers/routers.js
+++ server/routers/routers.js
@@ -7,9 +7,12 @@ const dataController = require('../controllers/controllers')
 router.get('/server', dataController.getData)
 router.delete('/server/:id', dataController.closeData)
 router.post('/server', dataController.insertClosedData)
+router.post('/server/hide', dataController.insertHideData)
 router.get('/closed', dataController.getClosedData)
 router.delete('/closed/:id', dataController.deleteFromClosed)
 router.post('/closed', dataController.insertIntoHome)
-router.post('/server/hide', dataController.insertHideData)
+router.post('/hided', dataController.insertIntoHome)
+router.get('/hided', dataController.getHidedData)
+router.delete('/hided/:id', dataController.deleteFromHided)
 
 module.exports = router
