b9f3a646120cf9755043e6b364098857ff038a54
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-22 17:29:05 +0300

    changed ButtonToClosed component

--- client/src/components/ButtonsRoute/ToClosed/ButtonToClosed.js
+++ client/src/components/ButtonsRoute/ToClosed/ButtonToClosed.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import closedIMG from '../../../img/closed.png'
 
+import Description from '../../Description/Description'
+
 import './ButtonToClosed.css'
 
 const ButtonToClosed = () => {
     return (
         <div className="button_to_closed">
+            <div className="description_closed">
+                <Description description={'Закрытые'} />
+            </div>
             <span>
                 <a href="http://localhost:3000/closed">
                     <img src={closedIMG} className="icon_to_closed" alt='Закрытые' />
