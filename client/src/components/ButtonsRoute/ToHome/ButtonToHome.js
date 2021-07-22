bed65039951455188e6cdddb781b9b50511d955b
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-22 17:29:43 +0300

    changed ButtonToHome component

--- client/src/components/ButtonsRoute/ToHome/ButtonToHome.js
+++ client/src/components/ButtonsRoute/ToHome/ButtonToHome.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import homeIMG from '../../../img/home.png'
 
+import Description from '../../Description/Description'
+
 import './ButtonToHome.css'
 
 const ButtonToHome = () => {
     return (
         <div className="button_to_home">
+            <div className="description_home">
+                <Description description={'На главную'} />
+            </div>
             <span>
                 <a href="http://localhost:3000/home">
                     <img src={homeIMG} className="icon_to_home" alt='На главную' /> 
