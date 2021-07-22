16d4997b6e5ae656e54de5a5c359279c7a1e992c
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-22 21:29:50 +0300

    added Prompt component

--- client/src/components/HideBlock/HideBlock.js
+++ client/src/components/HideBlock/HideBlock.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Prompt from '../Prompt/Prompt'
+
 import hide_img from '../../img/hide.png'
 import view_img from '../../img/view.png'
 
@@ -8,18 +10,28 @@ import './HideBlock.css'
 const hideBlock = (display, hide) => {
     if (window.location.pathname === '/home') 
         return (
-            <div className='hide_block'>
-                <span onClick={hide}>
-                    <img src={hide_img} className='icon' alt='скрыть' />
-                </span>
+            <div className="hide">
+                <div className='hide_block_prompt'>
+                    <Prompt description={'Скрыть'} />
+                </div>
+                <div className='hide_block'>
+                    <span onClick={hide}>
+                        <img src={hide_img} className='icon' alt='скрыть' />
+                    </span>
+                </div>
             </div>
         )
     if (window.location.pathname === '/hided') 
         return (
-            <div className='hide_block'>
-                <span onClick={display}>
-                    <img src={view_img} className='icon' alt='скрыть' />
-                </span>
+            <div className='restore'>
+                <div className='restore_block_prompt'>
+                    <Prompt description={'Показать'} />
+                </div>
+                <div className='restore_block'>
+                    <span onClick={display}>
+                        <img src={view_img} className='icon' alt='показать' />
+                    </span>
+                </div>
             </div>
         )
     else
