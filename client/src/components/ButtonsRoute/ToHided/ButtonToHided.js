fe4bf905f3228c2383b16fe43f458e358b8f44a2
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-22 17:29:31 +0300

    changed ButtonToHided component

--- client/src/components/ButtonsRoute/ToHided/ButtonToHided.js
+++ client/src/components/ButtonsRoute/ToHided/ButtonToHided.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import hidedIMG from '../../../img/hided.png'
 
+import Description from '../../Description/Description'
+
 import './ButtonToHided.css'
 
 const ButtonToHided = () => {
     return (
         <div className="button_to_hided">
+            <div className="description_hided">
+                <Description description={'Скрытые'} />
+            </div>
             <span>
                 <a href="http://localhost:3000/hided">
                     <img src={hidedIMG} className="icon_to_hided" alt='Скрытые' />
