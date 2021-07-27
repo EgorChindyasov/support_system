0f089a08e63c2067d2066955a209e7330a4c203e
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-27 21:23:54 +0300

    created state data

--- client/src/App.js
+++ client/src/App.js
@@ -15,25 +15,46 @@ function App() {
 
   const [searchData, setSearchData] = useState('')
 
+  const [data, setData] = useState([])
+
   const handleChange = (event) => {
         setSearchData(event.target.value)
   }
 
-    return (
+  console.log(searchData)
+
+  return (
     <BrowserRouter>
     <div className='App'>
       <Search 
         handleChange={handleChange} />
       <ButtonsRoute />
-        <Route 
-          path='/home' 
-          component={HomeFetch} 
-          searchData={searchData} />
+        <Route path='/home'>
+          <HomeFetch
+            data={data}
+            setData={setData} />
+        </Route>
         <Route path='/hided' component={HidedFetch} /> 
         <Route path='/closed' component={ClosedFetch} />
     </div>
     </BrowserRouter>
   )
+
+  //   return (
+  //   <BrowserRouter>
+  //   <div className='App'>
+  //     <Search 
+  //       handleChange={handleChange} />
+  //     <ButtonsRoute />
+  //       <Route 
+  //         path='/home' 
+  //         component={HomeFetch} 
+  //         searchData={searchData} />
+  //       <Route path='/hided' component={HidedFetch} /> 
+  //       <Route path='/closed' component={ClosedFetch} />
+  //   </div>
+  //   </BrowserRouter>
+  // )
 }
 
 export default App
