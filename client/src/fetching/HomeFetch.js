2f5509d11deb19ec72c9f82a458e4db1d37caf58
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-27 21:25:58 +0300

    added async function

--- client/src/fetching/HomeFetch.js
+++ client/src/fetching/HomeFetch.js
@@ -1,52 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import Home from '../pages/Home'
 
-const HomeFetch = ({ searchData }) => {
-    const [data, setData] = useState([])
+const HomeFetch = ({ data, setData }) => {
+    // const [data, setData] = useState([])
 
-    useEffect(() => {
-        fetch('/api/server')
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result)
-            }
-        )
-    }, [])
+    // useEffect(() => {
+    //     fetch('/api/server')
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result)
+    //         }
+    //     )
+    // }, [])
 
 // ---------------------------------------------------------
     
-    // useEffect(() => {
-    //     async function fetchAPI() {
-    //         let res = await fetch('/api/server')
-    //         res = await res.json()
-    //         setData(res)
-    //     }
-    //     fetchAPI()
-    // }, [])
+    useEffect(() => {
+        async function fetchAPI() {
+            let res = await fetch('/api/server')
+            res = await res.json()
+            setData(res)
+        }
+        fetchAPI()
+    }, [])
 
-    // return (
-    //     <Home data={data} />
-    // )
+    return (
+        <div>
+            <Home data={data} />
+        </div>
+    )
+}
 
 // --------------------------------------------------
 
-    return (
+//     return (
 
-        data.map(message => {
-            return (
-                      <Home 
-                        id={message.id}
-                        name={message.name}
-                        content={message.content} 
-                        importance={message.isitimportant}
-                        searchData={searchData} 
-                        key={message.id} 
-                      />
-                    )
-        })
-    )
-}
+//         data.map(message => {
+//             return (
+//                       <Home 
+//                         id={message.id}
+//                         name={message.name}
+//                         content={message.content} 
+//                         importance={message.isitimportant}
+//                         searchData={searchData} 
+//                         key={message.id} 
+//                       />
+//                     )
+//         })
+//     )
+// }
 
 export default HomeFetch
 
