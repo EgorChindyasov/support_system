50b842524bddab93c1ae20cdd333a98998c70091
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-29 14:18:23 +0300

    .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,72 +1,91 @@
 import React, { useState } from 'react'
 import InformBlock from '../components/InformBlock/InformBlock'
 
-// const Home = ({ data }) => {
+const Home = ({ data }) => {
 
-//   data.map(item => {
-//     return(
-//       <div className="AppBlock">
-//         <InformBlock 
-//           id={item.id} 
-//           name={item.name}
-//           content={item.content}
-//           importance={item.isitimportant}
-//           key={item.id} />
+  return (
+    <div className="AppBlock">
+    {data.map(item => {
+      return (
+        <InformBlock 
+          id={item.id} 
+                name={item.name}
+                content={item.content}
+                importance={item.isitimportant}
+                key={item.id} />
+      )
+    })}
+    </div>
+  )
+  // data.map(item => {
+  //   return(
+  //     <div className="AppBlock">
+  //       <InformBlock 
+  //         id={item.id} 
+  //         name={item.name}
+  //         content={item.content}
+  //         importance={item.isitimportant}
+  //         key={item.id} />
+  //     </div>
+  //   )
+  // })
+
+  return (
+    <div>
+      {data.map(item =>
+        item.name)
+      }
+    </div>
+  )
+  
+}
+
+// function Home ( {id, name, content, importance} ) {
+//   // состояние отображения блока
+//   const [showBlock, setShowBlock] = useState(true)
+
+//   const [hideBlock, setHideBlock] = useState(true)
+
+//   const display = () => setShowBlock(prev => !prev)
+
+//   const hide = () => setHideBlock(prev => !prev)
+    
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: {'Content-Type':'application/json'},
+//     body: JSON.stringify({ 
+//       name: `${name}`,
+//       content: `${content}`,
+//       isitimportant: `${importance}` })
+//   }
+
+//   if (!showBlock) {
+      
+//     fetch(`http://localhost:8080/api/server/${id}`, { method: 'DELETE' })
+//     fetch('/api/server', requestOptions)
+      
+//     return null
+//   }
+
+//   if (!hideBlock) {
+
+//     fetch(`http://localhost:8080/api/server/${id}`, { method: 'DELETE' })
+//     fetch('/api/server/hide', requestOptions)
+
+//     return null
+//   }
+
+//     return (
+//       <div className='AppBlock'>
+//         <InformBlock id={id} 
+//                      name={name} 
+//                      content={content} 
+//                      importance={importance}
+//                      display={display}
+//                      hide={hide}
+//                      key={id} />
 //       </div>
 //     )
-//   })
-//     // <div className="AppBlock">
-//     //   <InformBlock />
-//     // </div>
-  
 // }
-
-function Home ( {id, name, content, importance} ) {
-  // состояние отображения блока
-  const [showBlock, setShowBlock] = useState(true)
-
-  const [hideBlock, setHideBlock] = useState(true)
-
-  const display = () => setShowBlock(prev => !prev)
-
-  const hide = () => setHideBlock(prev => !prev)
-    
-  const requestOptions = {
-    method: 'POST',
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify({ 
-      name: `${name}`,
-      content: `${content}`,
-      isitimportant: `${importance}` })
-  }
-
-  if (!showBlock) {
-      
-    fetch(`http://localhost:8080/api/server/${id}`, { method: 'DELETE' })
-    fetch('/api/server', requestOptions)
-      
-    return null
-  }
-
-  if (!hideBlock) {
-
-    fetch(`http://localhost:8080/api/server/${id}`, { method: 'DELETE' })
-    fetch('/api/server/hide', requestOptions)
-
-    return null
-  }
-
-    return (
-      <div className='AppBlock'>
-        <InformBlock id={id} 
-                     name={name} 
-                     content={content} 
-                     importance={importance}
-                     display={display}
-                     hide={hide}
-                     key={id} />
-      </div>
-    )
-}
 
 export default Home
