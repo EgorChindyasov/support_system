cfc0f28f97da11ea2c2453308b6d08846203bf25
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-29 23:56:21 +0300

    added function setStyle() that changed style of a search pannel

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,13 +2,25 @@ import React from 'react'
 
 import './Search.css'
 
-const Search = ({ handleChange }) => {
+const Search = ({ handleChange, dataMatch }) => {
+
+    const setStyle = () => {
+        let style = {}
+        if (dataMatch) {
+            style = {
+                border: '1px solid red',
+                boxShadow: '0 0 3px red'
+            }
+        }
+        return style
+    }
 
     return (
             <input 
                 className='search' 
                 type='text' 
-                placeholder='Поиск' 
+                placeholder='Поиск'
+                style = {setStyle()} 
                 onChange={handleChange} />
     )
 }
