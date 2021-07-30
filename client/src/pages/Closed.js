f9b71e97f9ff9e8baa7ae3a91bf55176a16cdb0b
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:40:38 +0300

    added prop, GET-request and comments

--- client/src/pages/Closed.js
+++ client/src/pages/Closed.js
@@ -1,38 +1,49 @@
 import React, { useState } from 'react'
 import InformBlock from '../components/InformBlock/InformBlock'
 
-const URL = 'http://localhost:8080/api'
+import { URLClosed } from '../constants'
 
-function Closed ( {id, name, content, importance} ) {
-  // состояние отображения блока
+function Closed ( {id, name, content, importance, setClosedData} ) {
+  // true если оператор не нажал на кнопку восстановления, если нажал - false
   const [showBlock, setShowBlock] = useState(true)
 
+  // true если оператор не нажал на крестик, если нажал - false
   const [delBlock, setDelBlock] = useState(true)
 
   const display = () => setShowBlock(prev => !prev)
 
   const del = () => setDelBlock(prev => !prev)
     
+  // параметры POST-запроса
   const requestOptions = {
     method: 'POST',
     headers: {'Content-Type':'application/json'},
     body: JSON.stringify({ 
+      id: `${id}`,
       name: `${name}`,
       content: `${content}`,
-      isitimportant: `${importance}` })
+      importance: `${importance}` })
   }
 
+  // если оператор нажал на кнопку восстановления
   if (!showBlock) {
       
-    fetch(`${URL}/closed/${id}`, { method: 'DELETE' })
-    fetch('/api/closed', requestOptions)
+    fetch(`${URLClosed}/${id}`, { method: 'DELETE' })   // из таблицы deleted_data удаляем соответстующую запись
+    fetch(URLClosed, requestOptions)    // отправляем данные удаленного блока на сервер, заносим в таблицу data
+    fetch(URLClosed, {method: 'GET'})   // получаем обновленные данные из таблицы deleted_data
+      .then(res => res.json())
+      .then(data => setClosedData(data))
       
     return null
   }
 
+  // если оператор нажал на крестик
   if (!delBlock) {
     
-    fetch(`${URL}/closed/${id}`, { method: 'DELETE' })
+    fetch(`${URLClosed}/${id}`, { method: 'DELETE' })   // из таблицы deleted_data удаляем соответстующую запись
+    fetch(URLClosed, {method: 'GET'})   // получаем обновленные данные из таблицы deleted_data
+      .then(res => res.json())
+      .then(data => setClosedData(data))
 
     return null
   }
