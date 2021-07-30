b16b03c93bef827872f7510c89a9771d0dc6091b
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:40:09 +0300

    added prop, GET-request and comments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,40 +1,51 @@
 import React, { useState } from 'react'
 import InformBlock from '../components/InformBlock/InformBlock'
 
-const URL = 'http://localhost:8080/api'
+import { URLHome } from '../constants'
 
-function Home ( {id, name, content, importance} ) {
+function Home ({ id, name, content, importance, setHomeData }) {
   
-  // состояние отображения блока
+  // true если оператор не нажал на крестик, если нажал - false
   const [showBlock, setShowBlock] = useState(true)
 
+  // true если оператор не нажал на кнопку скрытия, если нажал - false
   const [hideBlock, setHideBlock] = useState(true)
 
   const display = () => setShowBlock(prev => !prev)
 
   const hide = () => setHideBlock(prev => !prev)
     
+  // параметры POST-запроса
   const requestOptions = {
     method: 'POST',
     headers: {'Content-Type':'application/json'},
     body: JSON.stringify({ 
+      id: `${id}`,
       name: `${name}`,
       content: `${content}`,
-      isitimportant: `${importance}` })
+      importance: `${importance}` })
   }
 
+  // если оператор нажал на крестик
   if (!showBlock) {
       
-    fetch(`${URL}/server/${id}`, { method: 'DELETE' })
-    fetch('/api/server', requestOptions)
+    fetch(`${URLHome}/${id}`, { method: 'DELETE' })   // из таблицы data удаляем соответстующую запись
+    fetch(URLHome, requestOptions)    // отправляем данные удаленного блока на сервер, заносим в таблицу deleted_data
+    fetch(URLHome, {method: 'GET'})   // получаем обновленные данные из таблицы data
+      .then(res => res.json())
+      .then(data => setHomeData(data))
       
     return null
   }
 
+  // если оператор нажал на кнопку скрытия
   if (!hideBlock) {
 
-    fetch(`${URL}/server/${id}`, { method: 'DELETE' })
-    fetch('/api/server/hide', requestOptions)
+    fetch(`${URLHome}/${id}`, { method: 'DELETE' })   // из таблицы data удаляем соответстующую запись
+    fetch(`${URLHome}/hide`, requestOptions)    // отправляем данные удаленного блока на сервер, заносим в таблицу hided_data
+    fetch(URLHome, {method: 'GET'})   // получаем обновленные данные из таблицы data
+      .then(res => res.json())
+      .then(data => setHomeData(data))
 
     return null
   }
