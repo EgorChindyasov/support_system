6876a11c0aadfa15d068249c0884e927c77c34fa
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:32:32 +0300

    added file constants.js that has url constants

---
+++ client/src/constants.js
@@ -0,0 +1,6 @@
+
+export const URLHome = 'http://localhost:8080/api/server'
+
+export const URLHided = 'http://localhost:8080/api/hided'
+
+export const URLClosed = 'http://localhost:8080/api/closed'
