eb75c269a8acc5d91e78774750e014a8145d43b8
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 16:50:04 +0300

    renamed Search to SearchPanel

---
+++ client/src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './SearchPanel.css'
+
+const Search = ({ handleChange, dataMatch }) => {
+
+    const setStyle = () => {
+        let style = {}
+        if (dataMatch) {
+            style = {
+                border: '1px solid red',
+                boxShadow: '0 0 3px red'
+            }
+        }
+        return style
+    }
+
+    return (
+            <input 
+                className='search' 
+                type='text' 
+                placeholder='Поиск'
+                style = {setStyle()} 
+                onChange={handleChange} />
+    )
+}
+
+export default Search
