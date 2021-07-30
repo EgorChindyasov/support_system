ed393ede44a9a6b595f53ff2161e685a8d6aeb89
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:36:08 +0300

    changed tables of DB

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -14,52 +14,52 @@ class DataController {
     }
 
     async insertClosedData(req, res) {
-        const {name, content, isitimportant} = req.body
-        const insertData = await db.query('insert into delete_data (name, content, isitimportant) values ($1, $2, $3) returning *', 
-                                         [name, content, isitimportant])
+        const {id, name, content, importance} = req.body
+        const insertData = await db.query('insert into deleted_data (id, name, content, importance) values ($1, $2, $3, $4) returning *', 
+                                         [id, name, content, importance])
         res.json(insertData.rows)
     }
 
     async insertHideData(req, res) {
-        const {name, content, isitimportant} = req.body
-        const insertData = await db.query('insert into hide_data (name, content, isitimportant) values ($1, $2, $3) returning *', 
-                                         [name, content, isitimportant])
+        const {id, name, content, importance} = req.body
+        const insertData = await db.query('insert into hided_data (id, name, content, importance) values ($1, $2, $3, $4) returning *', 
+                                         [id, name, content, importance])
         res.json(insertData.rows)
     }
 
     async getClosedData(req, res) {
-        const data = await db.query('select * from delete_data')
+        const data = await db.query('select * from deleted_data')
         res.json(data.rows)
     }
 
     async deleteFromClosed(req, res) {
         const id = req.params.id
-        const data = await db.query('delete from delete_data where id = $1', [id])
+        const data = await db.query('delete from deleted_data where id = $1', [id])
         res.json(data.rows)
     }
 
     async insertIntoHome(req, res) {
-        const {name, content, isitimportant} = req.body
-        const insertData = await db.query('insert into data (name, content, isitimportant) values ($1, $2, $3) returning *', 
-                                         [name, content, isitimportant])
+        const {id, name, content, importance} = req.body
+        const insertData = await db.query('insert into data (id, name, content, importance) values ($1, $2, $3, $4) returning *', 
+                                         [id, name, content, importance])
         res.json(insertData.rows)
     }
 
     async insertHideData(req, res) {
-        const {name, content, isitimportant} = req.body
-        const insertData = await db.query('insert into hide_data (name, content, isitimportant) values ($1, $2, $3) returning *', 
-                                         [name, content, isitimportant])
+        const {id, name, content, importance} = req.body
+        const insertData = await db.query('insert into hided_data (id, name, content, importance) values ($1, $2, $3, $4) returning *', 
+                                         [id, name, content, importance])
         res.json(insertData.rows)
     }
 
     async getHidedData(req, res) {
-        const data = await db.query('select * from hide_data')
+        const data = await db.query('select * from hided_data')
         res.json(data.rows)
     }
 
     async deleteFromHided(req, res) {
         const id = req.params.id
-        const data = await db.query('delete from hide_data where id = $1', [id])
+        const data = await db.query('delete from hided_data where id = $1', [id])
         res.json(data.rows)
     }
 }
