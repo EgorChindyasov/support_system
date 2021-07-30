ab9c02f02218b6b542a6f1e4b877850a9d316f63
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:37:32 +0300

    added props: serarchData and setHomeData

--- client/src/fetching/HomeFetch.js
+++ client/src/fetching/HomeFetch.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react'
 import Home from '../pages/Home'
 
-const HomeFetch = ({ data, setDataMatch }) => {
+const HomeFetch = ({ data, setDataMatch, searchData, setHomeData }) => {
 
     // data - массив объектов
-    
+
     useEffect(() => {
-      if (data.length === 0) {
+      if (data.length === 0 && searchData != 0) {
         setDataMatch(true)
       }
       else {
         setDataMatch(false)
       }
-    }, [data, setDataMatch])
+    }, [data, setDataMatch, searchData])
 
     return (
         data.map(message => {
@@ -21,7 +21,8 @@ const HomeFetch = ({ data, setDataMatch }) => {
                         id={message.id}
                         name={message.name} 
                         content={message.content} 
-                        importance={message.isitimportant} 
+                        importance={message.importance}
+                        setHomeData={setHomeData} 
                         key={message.id} 
                       />
                     )
