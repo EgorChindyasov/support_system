488afc8af663a59014353715b5bae240e5d6727f
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-30 14:40:22 +0300

    added prop, GET-request and comments

--- client/src/pages/Hided.js
+++ client/src/pages/Hided.js
@@ -1,39 +1,51 @@
 import React, { useState } from 'react'
 import InformBlock from '../components/InformBlock/InformBlock'
 
-const URL = 'http://localhost:8080/api'
+import { URLHided, URLHome } from '../constants'
 
-function Hided ( {id, name, content, importance} ) {
+function Hided ({ id, name, content, importance, setHidedData }) {
 
+  // true если оператор не нажал на кнопку отображения, если нажал - false
   const [displayBlock, setDisplayBlock] = useState(true)
 
+  // true если оператор не нажал на крестик, если нажал - false
   const [delBlock, setDelBlock] = useState(true)
 
   const display = () => setDisplayBlock(prev => !prev)
   
   const del = () => setDelBlock(prev => !prev)
     
+  // параметры POST-запроса
   const requestOptions = {
     method: 'POST',
     headers: {'Content-Type':'application/json'},
-    body: JSON.stringify({ 
+    body: JSON.stringify({
+      id: `${id}`, 
       name: `${name}`,
       content: `${content}`,
-      isitimportant: `${importance}` })
+      importance: `${importance}` })
   }
 
+  // если оператор нажал на кнопку отображения
   if (!displayBlock) {
       
-    fetch(`${URL}/hided/${id}`, { method: 'DELETE' })
-    fetch('/api/hided', requestOptions)
+    fetch(`${URLHided}/${id}`, { method: 'DELETE' })    // из таблицы hided_data удаляем соответстующую запись
+    fetch(URLHided, requestOptions)   // отправляем данные удаленного блока на сервер, заносим в таблицу data
+    fetch(URLHided, {method: 'GET'})   // получаем обновленные данные из таблицы hided_data
+      .then(res => res.json())
+      .then(data => setHidedData(data))
       
     return null
   }
 
+  // если оператор нажал на крестик
   if (!delBlock) {
     
-    fetch(`${URL}/hided/${id}`, { method: 'DELETE' })
-    fetch('/api/server', requestOptions)
+    fetch(`${URLHided}/${id}`, { method: 'DELETE' })    // из таблицы hided_data удаляем соответстующую запись
+    fetch(URLHome, requestOptions)    // отправляем данные удаленного блока на сервер, заносим в таблицу deleted_data
+    fetch(URLHided, {method: 'GET'})   // получаем обновленные данные из таблицы hided_data
+      .then(res => res.json())
+      .then(data => setHidedData(data))
 
     return null
   }
