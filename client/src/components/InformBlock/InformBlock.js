ba5b9fd9efde16451372fe0cce5c3ccd9f240124
Egor O. Chindyasov <egor@chindyasov.ru>
2021-07-31 21:37:17 +0300

    getted ControlPanelContext from App.js, created sorting

--- client/src/components/InformBlock/InformBlock.js
+++ client/src/components/InformBlock/InformBlock.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
 import Alert from '../Alert/Alert'
 import ContentBlock from '../ContentBlock/ContentBlock'
+
+import { ControlPanelContext } from '../App/App'
 
 import './InformBlock.css'
 
@@ -11,6 +13,23 @@ function InformBlock( {id, name, content, importance, display, del, hide} ) {
     const [alert, setAlert] = useState(false)
     // состояние наведения курсора на блок error
     const [hover, setHover] = useState(false)
+
+    // получаем sortPanelOption как контекст
+    // sortPanelOption хранит, какое поле выбрано в блоке сортировки
+    const { sortPanel } = useContext(ControlPanelContext)
+    const [ sortPanelOption ] = sortPanel
+
+    // рендерим блоки с полем importance = true
+    if (sortPanelOption === '1') {
+        if (importance === false)
+          return null
+      }
+    
+    // рендерим блоки с полем importance = false
+    if (sortPanelOption === '2') {
+        if (importance === true)
+          return null
+      }
 
     const toggleAlert = () => setAlert(prev => !prev)
 
@@ -35,23 +54,22 @@ function InformBlock( {id, name, content, importance, display, del, hide} ) {
 
     return (
         <AlertContext.Provider value={alert}>
-        <div className='inform_block'>
-            <div className='wrapper' 
-                 style={setStyle()} 
-                 onMouseEnter={toggleHover} 
-                 onMouseLeave={toggleHover}>
-                <ContentBlock toggle={toggleAlert} 
-                              name={name} 
-                              alert={alert} 
-                              importance={importance}
-                              display={display}
-                              del={del}
-                              hide={hide}/>
-                <Alert id={id} 
-                       content={content} 
-                       importance={importance} />
+            <div className='inform_block'>
+                <div className='wrapper' 
+                    style={setStyle()} 
+                    onMouseEnter={toggleHover} 
+                    onMouseLeave={toggleHover}>
+                    <ContentBlock toggle={toggleAlert} 
+                                name={name} 
+                                importance={importance}
+                                display={display}
+                                del={del}
+                                hide={hide}/>
+                    <Alert id={id} 
+                        content={content} 
+                        importance={importance} />
+                </div>
             </div>
-        </div>
         </AlertContext.Provider>
     )
 }
